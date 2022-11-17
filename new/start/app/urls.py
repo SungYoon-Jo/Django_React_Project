d56c7d8db19b0fb3f193ef01a7b773f39c89f655
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2022-11-17 12:50:26 +0900

    goodbye

--- new/start/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from app.views import Main,First
-
-urlpatterns = [
-    path('main/', Main.as_view()),
-    path('<str:idx>/', First.as_view()),
-]
